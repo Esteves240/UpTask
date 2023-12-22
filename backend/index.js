c5fc6219244c398632f08d04e3155003d171a3f1
Manuel Jose Medeiros Esteves <mjme.iefp@alunos.ipb.pt>
2023-12-22 16:37:48 +0000

    add routes get

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import conectarDB from "./config/db.js";
 import dotenv from "dotenv"; //para ocultar as credênciais da base de dados
+import usuarioRoutes from './routes/usuarioRoutes.js'
 
 const app = express();
 
@@ -8,8 +9,10 @@ dotenv.config();
 
 conectarDB();
 
-const PORT = process.env.PORT || 4000;
+//Routing
+app.use("/api/usuarios", usuarioRoutes);
 
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Servidor iniciado na porta ${PORT}`);
 });
