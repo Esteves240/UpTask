7b4a1fa717b9f1851e5e1f30d2a6bfd08cb45301
Manuel Jose Medeiros Esteves <mjme.iefp@alunos.ipb.pt>
2023-12-25 21:30:50 +0000

    Avoid duplicate registrys

--- backend/controllers/usuarioController.js
+++ backend/controllers/usuarioController.js
@@ -1,6 +1,14 @@
 import Usuario from "../models/Usuario.js";
 
 const resgistrar = async (req, res) => {
+    //Evitar registos duplicados
+    const existeUsuario = await Usuario.findOne({ email: req.body.email });
+    
+    if(existeUsuario){
+        const error = new Error('Usuario já resgistrado');
+        return res.status(400).json({ msg: error.message});
+    }
+
     try{
         const usuario = new Usuario(req.body)
         const usuarioAlmacenado = await usuario.save()
