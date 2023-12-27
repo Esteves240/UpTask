2305b4641894495c0b65f827f4749a82b67b1ab2
Manuel Jose Medeiros Esteves <mjme.iefp@alunos.ipb.pt>
2023-12-27 04:17:22 +0000

    Checking that the user is authenticated to access an Endpoint

--- backend/middleware/checkAuth.js
+++ backend/middleware/checkAuth.js
@@ -1,5 +1,29 @@
-const checkAuth = (req, res, next) => {
-    console.log('Desde checkAuth.js')
+import  jwt  from "jsonwebtoken";
+import Usuario from "../models/Usuario.js";
+
+const checkAuth = async (req, res, next) => {
+    let token;
+    if(
+        req.headers.authorization && req.headers.authorization.startsWith("Bearer")
+    ) {
+        try {
+            token = req.headers.authorization.split(' '[1]);
+            
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+            req.usuario = await Usuario.findById(decoded.id).select(
+                "-password -confirmado -token -createdAt -updatedAt -__v"
+                );
+
+            return next();
+        } catch (error) {
+            return res.status(404).json({msg: "Erro: Token expirado"})
+        }
+    }
+    if (!token){
+        const error = new Error("Token não é válida!");
+        res.status(401).json({ msg: error.message});
+    }
 
     next();
 };
