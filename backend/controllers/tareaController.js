c19ecff244418629940a72ebe5e04dfe01f7ae4e
Manuel Jose Medeiros Esteves <mjme.iefp@alunos.ipb.pt>
2023-12-28 17:39:58 +0000

    Updating a Task

--- backend/controllers/tareaController.js
+++ backend/controllers/tareaController.js
@@ -1,5 +1,5 @@
-import Proyecto from "../models/Proyecto,js";
-import Tarea from "../models/Tarea";
+import Proyecto from "../models/Proyecto.js";
+import Tarea from "../models/Tarea.js";
 
 const agregarTarea = async (req, res) => {
     const { proyecto } = rec.body;
@@ -29,8 +29,10 @@ const agregarTarea = async (req, res) => {
 };
 
 const obtenerTarea = async (req, res) => {
+    //identificar a tarefa
     const { id } = req.params;
 
+    //consular a existência da tarefa na base de dados
     const tarea = await Tarea.findById(id).populate('proyecto');
 
     //caso a tarefa não exista
@@ -48,7 +50,37 @@ const obtenerTarea = async (req, res) => {
     res.json(tarea);
 };
 
-const actualizarTarea = async (req, res) => {};
+const actualizarTarea = async (req, res) => {
+    //identificar a tarefa
+    const { id } = req.params;
+
+    //consular a existência da tarefa na base de dados
+    const tarea = await Tarea.findById(id).populate('proyecto');
+
+    //caso a tarefa não exista
+    if(!tarea){
+        const error = new Error('A tarefa não existe :(');
+        return res.status(404).json({msg: error.message});
+    }
+
+    //erro para quando não seja o criador a aceder à tarefa
+    if (tarea.proyecto.creador.toString() !== req.usuario._id.toString()) {
+        const error = new Error("Acção inválida!");
+        return res.status(403).json({ msg: error.message });
+    }
+
+    tarea.nombre = req.body.nombre || tarea.nombre;
+    tarea.descripcion = req.body.nombre || tarea.nombre;
+    tarea.prioridad = req.body.nombre || tarea.nombre;
+    tarea.fechaEntrega = req.body.nombre || tarea.nombre;
+
+    try {
+        const tareaAlmacenada = await tarea.save();
+        res.json(tareaAlmacenada);
+    } catch (error) {
+        console-log(error);
+    }
+};
 
 const eliminarTarea = async (req, res) => {};
 
