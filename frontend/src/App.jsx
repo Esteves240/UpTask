7528ee1836545e7a472adc274393270c2cd554e0
Manuel Jose Medeiros Esteves <mjme.iefp@alunos.ipb.pt>
2023-12-29 01:08:22 +0000

    Creating Routing with React Router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,25 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import AuthLayout from './layouts/AuthLayout.jsx'
+import Login from './paginas/Login.jsx'
+import Registrar from './paginas/Registrar.jsx'
+import OlvidePassword from './paginas/OlvidePassword.jsx'
+import NuevoPassword from './paginas/NuevoPassword.jsx'
+import ConfirmarCuenta from './paginas/ConfirmarCuenta.jsx'
 
 function App() {
   return (
-    <div className="App">
-      <h1>UpTask</h1>    
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<AuthLayout />}>
+          <Route index element={<Login />} />
+          <Route path='registrar' element={<Registrar />} />
+          <Route path='olvide-password' element={<OlvidePassword />} />
+          <Route path='olvide-password/:token' element={<NuevoPassword />} />
+          <Route path='confirmar/:token' element={<ConfirmarCuenta />} />
+        </Route>
+      </Routes>
+      
+    </BrowserRouter>
   )
 }
 
---
+++ frontend/src/paginas/Login.jsx
@@ -0,0 +1,9 @@
+//rafce
+
+const Login = () => {
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
