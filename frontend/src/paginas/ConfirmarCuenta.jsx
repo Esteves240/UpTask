e5e7267d262940e27d0d4f0e2004899cfb987375
Manuel Jose Medeiros Esteves <mjme.iefp@alunos.ipb.pt>
2023-12-29 14:58:52 +0000

    Creating the New Password and Confirm Account Pages

--- frontend/src/paginas/ConfirmarCuenta.jsx
+++ frontend/src/paginas/ConfirmarCuenta.jsx
@@ -1,6 +1,10 @@
 const ConfirmarCuenta = () => {
   return (
-    <div>ConfirmarCuenta</div>
+    <>
+      <h1 className="text-sky-600 font-black text-6xl capitalize">Confirma atua conta e começa a criar os teus teus
+        <span className="text-slate-700"> projectos</span>
+        </h1>
+    </>
   )
 }
 
